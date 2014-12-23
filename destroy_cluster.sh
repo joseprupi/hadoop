ad5417d199594934b8cc290c798837fec2c9b1f4
Josep Rubió <josep@datag.es>
2014-12-23 13:28:38 +0100

    ubuntu 12

---
+++ destroy_cluster.sh
@@ -0,0 +1,5 @@
+wget -qO- https://api.digitalocean.com/droplets/3557168/destroy/\?client_id\=e6477f4c77c213328184f33a95dde133\&api_key\=39f903cb08e24d0fba67c71b5f9b2def
+wget -qO- https://api.digitalocean.com/droplets/3557169/destroy/\?client_id\=e6477f4c77c213328184f33a95dde133\&api_key\=39f903cb08e24d0fba67c71b5f9b2def
+wget -qO- https://api.digitalocean.com/droplets/3557170/destroy/\?client_id\=e6477f4c77c213328184f33a95dde133\&api_key\=39f903cb08e24d0fba67c71b5f9b2def
+wget -qO- https://api.digitalocean.com/droplets/3557171/destroy/\?client_id\=e6477f4c77c213328184f33a95dde133\&api_key\=39f903cb08e24d0fba67c71b5f9b2def
+wget -qO- https://api.digitalocean.com/droplets/3557172/destroy/\?client_id\=e6477f4c77c213328184f33a95dde133\&api_key\=39f903cb08e24d0fba67c71b5f9b2def
